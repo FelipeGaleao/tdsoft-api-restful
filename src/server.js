3a7c908ff9bc190128af11d4fbaa3ca920197a75
Maycon Felipe Mota <mayconfelipemotamw3@gmail.com>
2023-05-13 11:40:19 -0300

    feat: adicionado testes unitarios conforme documentação

--- src/server.js
+++ src/server.js
@@ -1,11 +1,37 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const unique_repository = require('./data/unique_repository.json')
 
-app.get('/', (req, res) => {
-  res.send('Hello Wsssorld!')
+app.get('/repos/find', (req, res) => {
+  let nomeRepositorio = req.query.nome;
+  if (!nomeRepositorio) {
+    body = {
+      "mensagem": "Nome é obrigatório"
+    }
+    return res.status(400).send(body)
+  }
+  
+  body =  {
+    "id": "MDEwOlJlcG9zaXRvcnkxMDI3MDI1MA==",
+    "name": "facebook/react"
+  }
+  return res.status(200).send(body)
+  
 })
+
+app.get('/repos/:repoId', (req, res) => {
+  let idRepositorio = req.params.repoId;
+  if(idRepositorio){
+  body = unique_repository
+   return res.status(200).send(body)
+  }
+  return res.status(400).send({})
+})
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Aplicação sendo executada na porta: ${port}`)
 })
+
+module.exports = app;
---
+++ src/tests/test_repos.js
@@ -0,0 +1,39 @@
+var assert = require('assert');
+var request = require('supertest');
+var app = require('../server.js');
+
+// Teste de integração para o endpoint /repos/find 
+    // 400 - Não informar o nome do repositório 
+    describe('GET /repos/find', function() {
+        it('Buscar repo - Nome não informado', function(done) {
+            request(app).get('/repos/find')
+            .end(function(err, res) {
+                assert.equal(res.statusCode, 400);
+                assert.equal(res.body.mensagem, "Nome é obrigatório");
+                done();
+            });
+        })});
+
+    // 200 - Informar o nome do repositório
+    describe('GET /repos/find', function() {
+        it('Buscar repo - Nome informado', function(done) {
+            request(app).get('/repos/find')
+            .query({ nome: 'facebook/react' })
+            .end(function(err, res) {
+                assert.equal(res.statusCode, 200);
+                assert.equal(res.body.name, "facebook/react");
+                done();
+            });
+        })});
+
+// Teste de integração para o endpoint /repos/:repoId
+    // 200 - Informar o id do repositório
+    describe('GET /repos/:repoId', function() {
+        it('Buscar repo - Id informado', function(done) {
+            request(app).get('/repos/MDEwOlJlcG9zaXRvcnkxMDI3MDI1MA==')
+            .end(function(err, res) {
+                assert.equal(res.statusCode, 200);
+                assert.equal(res.body.id, "MDEwOlJlcG9zaXRvcnkxMDI3MDI1MA==");
+                done();
+            });
+        })});
